3f0441576d018506487af3c4d448ca352d281e61
RMuskovets <rmuskovets@gmail.com>
2018-10-10 18:00:24 +0300

    Add Javadoc to Range.java

--- src/org/roman/neurals/Range.java
+++ src/org/roman/neurals/Range.java
@@ -1,6 +1,18 @@
 package org.roman.neurals;
 
+/**
+ * @author Roman Myskovets'
+*/
+
 public class Range {
+    
+    /**
+     * Create an array that contains values in range from start to end.
+     * @param start The start of count.
+     * @param end The end of count.
+     * @param step The step.
+     * @returns The array.
+    */
 
     public static double[] range(double start, double end, double step) {
         int len = (int) ((start - end) / step);
