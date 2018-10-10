622dabf7c59bcf06c1010533bbc97f2ac4717693
RMuskovets <rmuskovets@gmail.com>
2018-10-10 18:03:00 +0300

    Add Javadoc to InputLayer.java

--- src/org/roman/neurals/InputLayer.java
+++ src/org/roman/neurals/InputLayer.java
@@ -1,15 +1,34 @@
 package org.roman.neurals;
 
+/**
+ * The input for a NeuralNetwork.
+ * @author Roman Myskovets'
+*/
+
 public class InputLayer {
+	
+	/**
+	 * The actual input for neural network.
+	*/
+	
 	private double[] inputs;
 	
 	public InputLayer(double[] inputs) {
 		this.inputs = inputs;
 	}
 	
+	/**
+	 * Get a value from inputs.
+	 * @param inputID the input index
+	*/
+	
 	public double get(int inputID) {
 		return inputs[inputID];
 	}
+	
+	/**
+	 * Get all inputs in a double array.
+	*/
 	
 	public double[] get() {
 		return inputs;
